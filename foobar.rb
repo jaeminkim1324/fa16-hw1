7308b7b4c7cdff810d7857800f85987a28bdf84d
Jae Min Kim <jaeminkim1324@berkeley.edu>
2016-09-20 16:15:31 -0700

    final version

--- foobar.rb
+++ foobar.rb
@@ -3,7 +3,29 @@ class Foobar
   def self.baz(a)
     # Class method
     # Call with `Foobar.baz`
+    arr = Array.new
+    sum = 0
+
+    a.each do |str|
+      str = str.to_i + 2
+      if (str <= 10 && str % 2 == 0)
+        arr.push(str)
+      end
+    end
+
+    arr.each do |lmao|
+      sum += lmao
+    end
+    return sum
+  end
+
+  def foo(b)
+    # Instance method
+    # Call with foobar_instance.foo
+  end
+
+  def bar(c)
+    # Instance method
+    # Call with foobar_instance.bar
   end
 end
-
-
--- hw1.rb
+++ hw1.rb
@@ -1,20 +1,22 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
-
+  return (a + b) ** 2
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
-
+  a.sort!.map do |b|
+    b + 1
+  end
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
-
+  return "#{first_name} #{last_name}"
 end
 
 def blockin_time(a)
   # DO NOT EDIT THIS CODE BELOW
-  require './foobar'
-  Foobar.baz a
+  require './foobar.rb'
+  Foobar.baz(a)
 end
